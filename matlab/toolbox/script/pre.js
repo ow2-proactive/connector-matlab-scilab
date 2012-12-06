9012f6c6fc5c9253b66558f5bbb6d14d301e4b9f
fviale <fabien.viale@activeeon.com>
2012-12-06 16:05:18 +0100

    MSC-215 - cannot import package error occurs in pre.js script when using UseJobClassPath option

--- matlab/toolbox/script/pre.js
+++ matlab/toolbox/script/pre.js
@@ -5,12 +5,15 @@
 
 importClass(java.lang.System);
 importClass(java.io.File);
-importClass(org.ow2.proactive.scheduler.common.util.FileUtils);
+importClass(java.lang.Class);
+
+
+importClass(org.apache.commons.io.FileUtils);
 importClass(org.ow2.proactive.scheduler.task.launcher.TaskLauncher);
-importClass(org.ow2.proactive.scheduler.ext.matlab.worker.MatlabExecutable);
-	
+
+
 var tmpDir = System.getProperty("java.io.tmpdir");
-	
+
 // Fix for SCHEDULING-1308: With RunAsMe on windows the forked jvm can have a non-writable java.io.tmpdir
 if (!new File(tmpDir).canWrite()) {
     throw new RuntimeException("Unable to execute task, java.io.tmpdir: " + tmpDir + " is not writable");
@@ -18,32 +21,43 @@ if (!new File(tmpDir).canWrite()) {
 
 // Creates dir <NODE_TMPDIR || SCRATCH_DIR>/MatlabForkedTasksTmp
 var rootDir = new File(tmpDir, "MatlabForkedTasksTmp");
-if ( !rootDir.exists() ){
-	if ( !rootDir.mkdir() ) {
-		throw new RuntimeException("Unable to execute task, unable to mkdir " + rootDir);
-	}
+if (!rootDir.exists()) {
+    if (!rootDir.mkdir()) {
+        throw new RuntimeException("Unable to execute task, unable to mkdir " + rootDir);
+    }
 }
+
+rootDir.setReadable(true, false);
+rootDir.setWritable(true, false);
+rootDir.setExecutable(true, false);
 
 // Get the task id
 var taskId = System.getProperty(TaskLauncher.SchedulerVars.JAVAENV_TASK_ID_VARNAME.toString());
 
 // Create task specific dir <NODE_TMPDIR || SCRATCH_DIR>/MatlabForkedTasksTmp/Task<id>
-var taskIdDir = new File(rootDir, "Task"+taskId);
+var taskIdDir = new File(rootDir, "Task" + taskId);
 if (taskIdDir.exists()) {
-	// Delete previous data 
-	taskIdDir["delete"](); // avoids conflict with 'delete' keyword
-	FileUtils.removeDir(taskIdDir);
+    // Delete previous data
+    FileUtils.deleteDirectory(taskIdDir);
 }
-if ( !taskIdDir.mkdir() ) {
-	throw new RuntimeException("Unable to execute task, unable to mkdir " + taskIdDir);
+if (!taskIdDir.mkdir()) {
+    throw new RuntimeException("Unable to execute task, unable to mkdir " + taskIdDir);
 }
 
-System.setProperty(MatlabExecutable.MATLAB_TASK_TMPDIR, taskIdDir);
+taskIdDir.setReadable(true, false);
+taskIdDir.setWritable(true, false);
+taskIdDir.setExecutable(true, false);
+
+System.setProperty("matlab.task.tmpdir", taskIdDir);
 
 // Create dir for MATLAB preferences
 var matlabPrefdir = new File(taskIdDir, "MatlabPrefdir");
-if ( !matlabPrefdir.mkdir() ){
-	throw new RuntimeException("Unable to execute task, unable to mkdir " + matlabPrefdir);
+if (!matlabPrefdir.mkdir()) {
+    throw new RuntimeException("Unable to execute task, unable to mkdir " + matlabPrefdir);
 }
+matlabPrefdir.setReadable(true, false);
+matlabPrefdir.setWritable(true, false);
+matlabPrefdir.setExecutable(true, false);
 matlabPrefdir.deleteOnExit();
-System.setProperty(MatlabExecutable.MATLAB_PREFDIR, matlabPrefdir.getAbsolutePath());
+
+System.setProperty("matlab.task.tmpdir", matlabPrefdir.getAbsolutePath());
