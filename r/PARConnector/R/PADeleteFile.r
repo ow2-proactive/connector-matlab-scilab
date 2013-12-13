dde23271e48ed2c4ef5f14b842bce53d697ad9dd
fviale <fabien.viale@activeeon.com>
2013-12-13 18:56:18 +0100

    minor fix in PADeleteFile, updated client
    Change-Id: I4af4d1d879b53cf6c9055cfd40d936802bfa0292

--- r/PARConnector/R/PADeleteFile.r
+++ r/PARConnector/R/PADeleteFile.r
@@ -1,11 +1,11 @@
 PADeleteFile <- function(space, pathname,
-                         client = .scheduler.client) {
+                         client = PAClient()) {
   deleted <- FALSE
   j_try_catch(
     deleted <- J(client, "deleteFile", .getSpaceName(space), pathname),     
-    .handler = function(e,.print.error,.default.handler) {
+    .handler = function(e,.print.error) {
       print(str_c("Error in PADeleteFile(",space,",",pathname,") :"))
-      .default.handler(e,.print.error)
+      PAHandler(e,.print.error)
     })
   
   return (deleted)
