97dc324df469f9bbacfffd92418ec9a6c07a0a42
Vladimir Bodnartchouk <Vladimir.Bodnartchouk@activeeon.com>
2013-12-26 17:44:05 +0100

    Added check, build and remove previous steps ...

--- r/build-and-test.groovy
+++ r/build-and-test.groovy
@@ -7,37 +7,83 @@
 // - runs the integration test that uses PARConnector api functions and submit jobs
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Check that the current dir is matlab_scilab_connector
-def rtoolboxDir = new File('r');
-assert rtoolboxDir.exists() : '!!! Unable to locate the r dir, please run this script from the correct location !!!'
-
 def fs = File.separator
+
+// Check that the current dir is 'r' or its parent
+def homeDir = new File(System.getProperty('user.dir'));
+if ('r' != homeDir.getName()) {
+	rDir = new File(homeDir, 'r')
+	if (rDir.exists()) {
+		homeDir = rDir
+	} else {
+		'!!! Please run the script from r dir or from the parent of r dir !!!'
+	}
+}
+
+def parConnectorDir = new File(homeDir, 'PARConnector')
+assert parConnectorDir.exists() : '!!! Unable to PARConnector dir !!!'
+
 def schedHome = System.getenv()['SCHEDULER_340']
 assert schedHome != null : '!!! Unable to locate Scheduler 3.4.0 home dir, the SCHEDULER_340 env var is undefined !!!'
 
 def rHome = System.getenv()['R_HOME']
 assert rHome != null : '!!! Unable to locate R home dir, the R_HOME env var is undefined !!!'
 
+def rLibraryDir = new File(rHome,'library') // check if on linux it works
+def rLibraryPath = rLibraryDir.getAbsolutePath()
+// todo check for rJava
+
 // ! THIS IS A FIX FOR rJava that requires JAVA_HOME to be the location of the JRE !
-def currentEnv = System.getenv();
-currentEnv['JAVA_HOME'] = System.getenv()['JAVA_HOME']+fs+'jre';
 def newEnv = []
-currentEnv.each() {k,v -> newEnv << k+'='+'v'}
+System.getenv().each() {k,v ->
+	if ('JAVA_HOME'.equals(k)) { v = v+fs+'jre' }	
+	newEnv << k+'='+v
+}
 
-println '----> new env = ' + newEnv
-
-def paArch = System.getenv()['PROGRAMFILES(X86)'] != null ? 'x64' : 'i386'
+def paArch = System.getenv()['ProgramFiles(x86)'] != null ? 'x64' : 'i386'
 def rExe = rHome+fs+'bin'+fs+paArch+fs+'R.exe'
 
 println '\n######################\n#   CHECKING R packages from package sources ... \n######################'
-
-// Delete previous checks
-(new File(rtoolboxDir, 'PARConnector.Rcheck')).deleteDir(); 
-def rcheckProcess = [rExe, 'CMD', 'check', '--no-codoc', '--no-manual', '--no-multiarch', 'PARConnector'].execute(newEnv, rtoolboxDir)
+(new File(homeDir, 'PARConnector.Rcheck')).deleteDir()
+def rcheckProcess = [rExe, 'CMD', 'check', '--no-codoc', '--no-manual', '--no-multiarch', 'PARConnector'].execute(newEnv, homeDir)
 rcheckProcess.inputStream.eachLine {
 	println '>> ' + it
 }
-rcheckProcess.waitFor()
+assert rcheckProcess.waitFor() == 0 : "!!! It seems R CHECK failed !!!"
+
+println '\n######################\n#   BUILDING PARConnector ... \n######################'
+def distDir = new File(homeDir, 'dist')
+distDir.deleteDir()
+distDir.mkdir()
+def rbuildProcess = [rExe, 'CMD', 'build', parConnectorDir.getAbsolutePath()].execute(newEnv, distDir)
+rbuildProcess.inputStream.eachLine {
+	println '>> ' + it
+}
+assert rbuildProcess.waitFor() == 0 : "!!! It seems R BUILD failed !!!"
+
+println '\n######################\n#   REMOVING previous PARConnector ... \n######################'
+def removeProcess = [rExe, 'CMD', 'REMOVE', '--library='+rLibraryPath, 'PARConnector'].execute(newEnv, homeDir)
+removeProcess.inputStream.eachLine {
+	println '>> ' + it
+}
+assert removeProcess.waitFor() == 0 : "!!! It seems REMOVING previous failed !!!"
+
+println '\n######################\n#   INSTALLING PARConnector ... \n######################'
+//for /f "tokens=*" %%a in ('dir /b /od') do set newest=%%a
+//%R_EXE% CMD INSTALL  --library="%R_HOME%\library" %newest%
+//def removeProcess = [rExe, 'CMD', 'INSTALL', '--preclean', '--no-test-load', '--library='+rLibraryPath, 'PARConnector'].execute(newEnv, homeDir)
+//removeProcess.inputStream.eachLine {
+//	println '>> ' + it
+//}
+//assert removeProcess.waitFor() == 0 : "!!! It seems R BUILD failed !!!"
+
+/*
+echo ************************************************************
+echo ********* RUNNING integration tests                *********
+echo ************************************************************
+%R_EXE% --vanilla < ..\tests\test.R
+*/
+
 
 //rem %R_EXE% CMD check --no-codoc --no-manual --no-multiarch PARConnector
 //IF ERRORLEVEL 1 GOTO :exit
