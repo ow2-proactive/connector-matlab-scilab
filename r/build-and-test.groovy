087236c0b3a46de51981baa935f087f0f8192281
Vladimir Bodnartchouk <Vladimir.Bodnartchouk@activeeon.com>
2013-12-26 15:48:43 +0100

    Added groovy script to build and test

---
+++ r/build-and-test.groovy
@@ -0,0 +1,66 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Integration test for the PARConnector with Scheduler 3.4.0
+// 
+// - builds the PARConnector
+// - copies the contents to the scheduler's home addons directory
+// - starts the rm+scheduler+4nodes using the standard start-server.js script
+// - runs the integration test that uses PARConnector api functions and submit jobs
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Check that the current dir is matlab_scilab_connector
+def rtoolboxDir = new File('r');
+assert rtoolboxDir.exists() : '!!! Unable to locate the r dir, please run this script from the correct location !!!'
+
+def fs = File.separator
+def schedHome = System.getenv()['SCHEDULER_340']
+assert schedHome != null : '!!! Unable to locate Scheduler 3.4.0 home dir, the SCHEDULER_340 env var is undefined !!!'
+
+def rHome = System.getenv()['R_HOME']
+assert rHome != null : '!!! Unable to locate R home dir, the R_HOME env var is undefined !!!'
+
+// ! THIS IS A FIX FOR rJava that requires JAVA_HOME to be the location of the JRE !
+def javaHome = System.getenv()['JAVA_HOME']+fs+'jre'
+def paArch = System.getenv()['PROGRAMFILES(X86)'] != null ? 'x64' : 'i386'
+def rExe = rHome+fs+bin+fs+paArch+fs+'R.exe'
+def env = ['R_HOME='+rHome, 'JAVA_HOME='+javaHome]
+
+println '\n######################\n#   CHECKING R packages from package sources ... \n######################'
+
+// Delete previous checks
+(new File(rtoolboxDir, 'PARConnector.Rcheck')).deleteDir(); 
+def rcheckProcess = [rExe, 'CMD', 'check', '--no-codoc', '--no-manual', '--no-multiarch', 'PARConnector'].execute(env, rtoolboxDir)
+rcheckProcess.inputStream.eachLine {
+	println '>> ' + it
+}
+rcheckProcess.waitFor()
+
+//rem %R_EXE% CMD check --no-codoc --no-manual --no-multiarch PARConnector
+//IF ERRORLEVEL 1 GOTO :exit
+
+/*
+try {   
+	println '\n######################\n#   Starting the Scheduler using start-server.js ... \n######################'
+	schedProcess = ["jrunscript", "start-server.js"].execute(null, new File(schedHome+'/bin'))
+	try {
+		schedProcess.inputStream.eachLine {
+			println '>> ' + it
+			if (it.contains('terminate all')) {	
+				throw new Exception('ready')
+			}
+		}
+		} catch (e) {}
+
+		} finally {
+			println '\n######################\n#   Shutting down the Scheduler ... \n######################'    
+			try { 
+				def stdin = schedProcess.getOutputStream();
+				stdin.write('exit\n'.getBytes() );
+				stdin.flush();
+				} catch (e) {e.printStackTrace()}
+				calcServerProcess.waitForOrKill(500)
+				schedProcess.waitFor();
+			}
+		}
+	}
+}
+*/
--- r/tests/test.R
+++ r/tests/test.R
@@ -1,30 +1,53 @@
 # source('H:/Temp/workspace/R-dev/connectors/matlab_scilab_connector/r/tests/test.R');
 
 {
-library("PARConnector");
+  library("PARConnector");
+  
+  cat("*** Locating Scheduler home ***","\n");  
+  schedHomePath = Sys.getenv("SCHEDULER_HOME");
+  
+  if ( file.exists(schedHomePath) ){
+    cat("Using Scheduler:", schedHomePath,"\n");
+  }  else {    
+    sched_dirname = "ProActiveScheduling-3.4.0_bin_full";
+    if ( file.exists(sched_dirname) ){      
+      cat("Reusing existing Scheduler");
+    } else {
+      print("Downloading from www.activeoon.com");
+      #sched_file <- paste(getwd(), '/sched.zip', sep="")
+      #download.file("http://www.activeeon.com/public_content/releases/ProActive/3.4.0/ProActiveScheduling-3.4.0_bin_full.zip",
+      #              sched_file,
+      #              "auto",
+      #              quiet = FALSE, mode = "w",
+      #              cacheOK = TRUE,
+      #              extra = getOption("download.file.extra"))
+      print("Unzipping ...");
+      #unzip(sched.zip);
+    }  
+  }
+  
+  cat("*** Starting the Scheduler ***","\n");
+  #process = system(
+  #  paste(
+  #    "cmd.exe", "/c", "cd", "/d",
+  #    paste(schedHomePath,"\\bin", sep=""),
+  #    "&&", "jrunscript", "start-server.js", sep=" "), wait = FALSE);
+  
+  cat(process, "\n");
+  
+  cat("*** Trying to connect to http://localhost:8080/rest/rest ***","\n")  
+  nbTries <- 0;
 
-print("*** Starting the Scheduler ***");
-
-sched_dirname = "/ProActiveScheduling-3.4.0_bin_full";
-
-if ( file.exists(sched_dirname) ){
-  print("Reusing existing Scheduler");
-} else {
-  print("Downloading from www.activeoon.com");
-  #sched_file <- paste(getwd(), '/sched.zip', sep="")
-  #download.file("http://www.activeeon.com/public_content/releases/ProActive/3.4.0/ProActiveScheduling-3.4.0_bin_full.zip",
-  #              sched_file,
-  #              "auto",
-  #              quiet = FALSE, mode = "w",
-  #              cacheOK = TRUE,
-  #              extra = getOption("download.file.extra"))
-  print("Unzipping ...");
-  #unzip(sched.zip);
-}
-
-print("*** TEST ***");
-#source('../rest_client/pa_connect.R');
-
-#PAConnect(url='http://localhost:8080/rest/rest', login='demo', pwd='demo')
-
+  repeat {
+    nbTries <- nbTries + 1;
+    x <- tryCatch({
+      if(nbTries == 10) {
+        cat("*** After ", nbTries, " tries unable to connect, existing ..." , "\n");
+        break();
+      }
+      PAConnect(url='http://localhost:8080/rest/rest', login='demo', pwd='demo');
+      cat("Sucessfully connected !!", "\n");
+      break();
+    }, error = function(e) e)  
+  }
 }
