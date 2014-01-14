9ce84dee7262963d02411983d6956ff89445a2e0
Vladimir Bodnartchouk <Vladimir.Bodnartchouk@activeeon.com>
2014-01-14 18:43:46 +0100

    Added temporary fiels removal in the .Last function and removed .txt in filenames in testWorkflowIOFiles.R

--- r/tests/testPushPull.r
+++ r/tests/testPushPull.r
@@ -7,11 +7,14 @@ if(!file.exists("foo.txt")) {
   file.create("foo.txt", showWarnings = TRUE)
 }
 
-
 if(file.exists("foo2.txt")) {
   file.remove("foo2.txt")
 }
 
+.Last <- function() {
+  file.remove("foo.txt")
+  file.remove("foo2.txt")
+}
 
 PAPushFile("USER", "/", "foo.txt", "foo.txt") 
 cat("file pushed !!", "\n");
@@ -19,11 +22,6 @@ cat("file pushed !!", "\n");
 PAPullFile("USER", "/foo.txt", "foo2.txt") 
 cat("file pulled !!", "\n");
 
-
 if(!file.exists("foo2.txt")) {
-  msg <- "Error, can't find file foo2.txt"
-  stop(msg) 
+  stop("Error, can't find file foo2.txt")
 }
-
-
-
--- r/tests/testWorkflowIOFiles.R
+++ r/tests/testWorkflowIOFiles.R
@@ -1,3 +1,5 @@
+source("tests/utils.r")
+
 library("PARConnector");
 cat("*** Trying to connect to http://localhost:8080/rest/rest ***","\n") 
 PAConnect(url='http://localhost:8080/rest/rest', login='demo', pwd='demo');
@@ -7,12 +9,12 @@ cat("Sucessfully connected !!", "\n");
 
 split <- function(ind) {
   listoutfiles <- NULL
-  if (!file.exists("in.txt")) {
-    stop("in.txt", " does not exist")
+  if (!file.exists("in")) {
+    stop("The file 'in' does not exist")
   }
   for (i in ind) {
-    outf <- paste0("split_",i,".txt")
-    file.copy("in.txt",outf)
+    outf <- paste0("split_",i)
+    file.copy("in",outf)
     listoutfiles <- c(listoutfiles,outf)
   }
   return(listoutfiles)
@@ -26,68 +28,70 @@ compute <- function(infile) {
 
 merge <- function(...) {
   dots <- list(...)
-  file.create("out.txt")
+  file.create("out")
   for (i in 1:length(dots)) {
-    file.append("out.txt",dots[[i]])
+    file.append("out",dots[[i]])
   }
-  return("out.txt")
-} 
+  return("out")
+}
+
+.Last <- function() {  
+  file.remove("in")
+  file.remove("out")
+  removeFiles("split_", 4)
+  removeFiles("out_", 4)
+}
 
 # testing with numeric indexes in file names
 
-if(!file.exists("in.txt")) {
-  file.create("in.txt", showWarnings = TRUE)
+if(!file.exists("in")) {
+  file.create("in", showWarnings = TRUE)
 }
 
-
-if(file.exists("out.txt")) {
-  file.remove("out.txt")
+if(file.exists("out")) {
+  file.remove("out")
 }
 
 r=PASolve(
   PAM(merge,
       PA(compute,
-         PAS(split, 1:4, input.files="in.txt", output.files="split_%1%.txt"),
-         input.files="split_%1%.txt",
-         output.files="out_%1%.txt"),
-      input.files="out_%1:4%.txt",
-      output.files="out.txt")
+         PAS(split, 1:4, input.files="in", output.files="split_%1%"),
+         input.files="split_%1%",
+         output.files="out_%1%"),
+      input.files="out_%1:4%",
+      output.files="out")
 ) 
 
 val <- PAWaitFor(r)
 print(val)
 
-if (val[["t6"]] != "out.txt") {
-  msg <- paste0("val[t6]=",val[["t6"]], " should be out.txt\n")
-  stop(msg) 
+if (val[["t6"]] != "out") {
+  stop("val[t6]=", val[["t6"]], " should be out\n")  
 }
 
 # testing with character indexes in file names
 
-if(!file.exists("in.txt")) {
-  file.create("in.txt", showWarnings = TRUE)
+if(!file.exists("in")) {
+  file.create("in", showWarnings = TRUE)
 }
 
-
-if(file.exists("out.txt")) {
-  file.remove("out.txt")
+if(file.exists("out")) {
+  file.remove("out")
 }
 
 r=PASolve(
   PAM(merge,
       PA(compute,
-         PAS(split, c("a","b","c","d"), input.files="in.txt", output.files="split_%1%.txt"),
-         input.files="split_%1%.txt",
-         output.files="out_%1%.txt"),
-      input.files="out_%1:4%.txt",
-      output.files="out.txt")
+         PAS(split, c("a","b","c","d"), input.files="in", output.files="split_%1%"),
+         input.files="split_%1%",
+         output.files="out_%1%"),
+      input.files="out_%1:4%",
+      output.files="out")
 ) 
 
 val <- PAWaitFor(r)
 print(val)
 
-if (val[["t6"]] != "out.txt") {
-  msg <- paste0("val[t6]=",val[["t6"]], " should be out.txt\n")
-  stop(msg) 
+if (val[["t6"]] != "out") {
+  stop("val[t6]=", val[["t6"]], " should be out\n")
 }
-
