830797426fbf86b470883e6b14d287ea33562407
Vladimir Bodnartchouk <Vladimir.Bodnartchouk@activeeon.com>
2014-01-30 20:37:34 +0100

    Fix parscript location of R_HOME for Mac

--- r/parscript/src/org/ow2/parscript/PARScriptFactory.java
+++ r/parscript/src/org/ow2/parscript/PARScriptFactory.java
@@ -3,6 +3,7 @@ package org.ow2.parscript;
 import static javax.script.ScriptEngine.NAME;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Collections;
@@ -21,8 +22,8 @@ import org.rosuda.jrs.RScriptFactory;
  */
 public final class PARScriptFactory extends RScriptFactory {
 	public static final String ENGINE_NAME = "parscript";
-	public static final boolean isWindows = System.getProperty("os.name")
-			.startsWith("Windows");
+	public static final boolean isWindows = System.getProperty("os.name").toLowerCase().contains("win");
+	public static final boolean isMac = System.getProperty("os.name").toLowerCase().contains("mac");
 
 	@Override
 	public String getEngineName() {
@@ -50,48 +51,64 @@ public final class PARScriptFactory extends RScriptFactory {
 		// Check if the path to rJava is already setted
 		String libPath = System.getProperty("java.library.path");
 		if (libPath == null || !libPath.contains("jri")) {
-			this.setLibraryPath();
+			try {
+				this.setLibraryPath();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		return PARScriptEngine.create();
 	}
-
+	
+	/** Tries to locate from registry or usual paths then adds rhome to the library path */
 	private void setLibraryPath() {
 		String rHome = System.getenv("R_HOME");
-		if (StringUtils.isBlank(rHome)) {
-			if (isWindows) {
+		boolean isBlank = StringUtils.isBlank(rHome);
+		if (isWindows) {
+			// On Windows try to locate from Windows Registry
+			if (isBlank) {
 				try {
 					rHome = WinRegistry.readString(
 							WinRegistry.HKEY_LOCAL_MACHINE, // HKEY
 							"SOFTWARE\\R-core\\R", // Key
 							"InstallPath");
-				} catch (Exception e) {
-					e.printStackTrace();
-					throw new IllegalStateException(
-							"Unable to locate R homedir, it seems R is not installed", e);
-				}
-			} else {
-				File usual = new File("/usr/lib/R");
-				if (usual.exists()) {
-					rHome = usual.getAbsolutePath();
-					// Set the current process 'R_HOME' environment variable 
-					try {
-						Environment.setenv("R_HOME", rHome, true);
-					} catch (Exception e) {
-						throw new IllegalStateException(
-								"Unable to set R_HOME environment variable to " + rHome, e);
+					if (!(new File(rHome).exists())) {
+						throw new FileNotFoundException("The path " + rHome + " given by the Windows Registry key does not exists");
 					}
+					Environment.setenv("R_HOME", rHome, true);
+				} catch (Exception e) {
+					throw new IllegalStateException("Unable to locate R homedir from Windows Registry, it seems R is not installed, please define the R_HOME env variable", e);
 				}
 			}
-			if (rHome == null) {
-				throw new IllegalStateException(
-						"Unable to locate R homedir, be sure the R_HOME env variable is defined");
-			}
-		}
-		// Add the library path
-		if (isWindows) {
 			dynamicAddLibraryPathWindows(rHome);
+		} else if (isMac) {
+			// On Mac try to locate from usual install path
+			if (isBlank) {
+				rHome = "/Library/Frameworks/R.framework/Resources";
+				try {
+					if (!(new File(rHome).exists())) {
+						throw new FileNotFoundException("The usual " + rHome + " path does not exists");
+					}
+					Environment.setenv("R_HOME", rHome, true);
+				} catch (Exception e) {
+					throw new IllegalStateException("Unable to locate R homedir, the R_HOME env variable must be defined", e);
+				}
+			}
+			dynamicAddLibraryPathLinuxMac(rHome, "library");
 		} else {
-			dynamicAddLibraryPathLinux(rHome);
+			// On Linux try to locate from usual install path
+			if (isBlank) {
+				rHome = "/usr/lib/R";
+				try {
+					if (!(new File(rHome).exists())) {
+						throw new FileNotFoundException("The usual " + rHome + " path does not exists");
+					}
+					Environment.setenv("R_HOME", rHome, true);
+				} catch (Exception e) {
+					throw new IllegalStateException("Unable to locate R homedir, the R_HOME env variable must be defined", e);
+				}
+			}			
+			dynamicAddLibraryPathLinuxMac(rHome, "site-library");
 		}
 	}
 
@@ -99,18 +116,24 @@ public final class PARScriptFactory extends RScriptFactory {
 		String fs = java.io.File.separator;
 		// Get the architecture of the jvm not the os
 		String sunArchDataModel = System.getProperty("sun.arch.data.model");
-		String rLibrarayPath = rHome + fs + "bin" + fs;
-		String rJavaPath = rHome + fs + "library" + fs + "rJava";
+		String rLibraryPath = rHome + fs + "bin" + fs;
+		String packagesLibraryPath = rHome + fs + "library";
+		// If R_LIBS env var is defined locate rJava there
+		String rLibs = System.getenv("R_LIBS");
+		if (!StringUtils.isBlank(rLibs)) {
+			packagesLibraryPath = rLibs;
+		}
+		String rJavaPath = packagesLibraryPath + fs + "rJava";
 		if (!new File(rJavaPath).exists()) {
-			throw new IllegalStateException("Unable to locate rJava in " + rJavaPath);
+			throw new IllegalStateException("Unable to locate rJava in " + rJavaPath + " the R_LIBS env variable must be defined");
 		}
 		String jriLibraryPath = rJavaPath + fs + "jri" + fs;
 		// Use correct libraries depending on jvm architecture 
 		if ("32".equals(sunArchDataModel)) {
-			rLibrarayPath += "i386";
+			rLibraryPath += "i386";
 			jriLibraryPath += "i386";
 		} else if ("64".equals(sunArchDataModel)) {
-			rLibrarayPath += "x64";
+			rLibraryPath += "x64";
 			jriLibraryPath += "x64";
 		}
 		// Dynamically add to java library path
@@ -123,22 +146,24 @@ public final class PARScriptFactory extends RScriptFactory {
 		// Update the current process 'Path' environment variable
 		try {
 			String varValue = System.getenv("Path");
-			Environment.setenv("Path", varValue + File.pathSeparator + rLibrarayPath, true);
+			Environment.setenv("Path", varValue + File.pathSeparator + rLibraryPath, true);
 		} catch (Exception e) {
 			throw new IllegalStateException(
-					"Unable to add R lib to Path environment variable " + rLibrarayPath, e);
+					"Unable to add R lib to Path environment variable " + rLibraryPath, e);
 		}
 	}
-	
-	private static void dynamicAddLibraryPathLinux(final String rHome) {
+
+	private static void dynamicAddLibraryPathLinuxMac(final String rHome, String defaultPackagesDirname) {
 		String fs = java.io.File.separator;
-		String rJavaPath = rHome + fs + "site-library" + fs + "rJava";
+		String packagesLibraryPath = rHome + fs + defaultPackagesDirname;
+		// If R_LIBS env var is defined locate rJava there
+		String rLibs = System.getenv("R_LIBS");
+		if (!StringUtils.isBlank(rLibs)) {
+			packagesLibraryPath = rLibs;
+		}
+		String rJavaPath = packagesLibraryPath + fs + "rJava";
 		if (!new File(rJavaPath).exists()) {
-			Exception e = new IllegalStateException("Unable to locate rJava in " + rJavaPath);
-			rJavaPath = rHome + fs + "library" + fs + "rJava";
-			if (!new File(rJavaPath).exists()) {
-				throw new IllegalStateException("Unable to locate rJava in " + rJavaPath, e);
-			}
+			throw new IllegalStateException("Unable to locate rJava package in " + rJavaPath + " the R_LIBS env variable must be defined");
 		}
 		// Dynamically add to java library path
 		String jriLibraryPath = rJavaPath + fs + "jri" + fs;
@@ -148,7 +173,8 @@ public final class PARScriptFactory extends RScriptFactory {
 			throw new IllegalStateException(
 					"Unable to add jri to library path " + jriLibraryPath, e);
 		}
-	}
+	}	
+
 	/**
 	 * Adds the specified path to the java library path
 	 * 
