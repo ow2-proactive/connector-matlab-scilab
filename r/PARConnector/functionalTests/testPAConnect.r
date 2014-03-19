83b94ce1976a72e6b940d1e5c66cb249e880ad99
fviale <fabien.viale@activeeon.com>
2014-03-19 11:03:24 +0100

    Remove R connector from Matlab/Scilab connector repository
    Change-Id: Ib1a4e0302d7c836976905a8ff8ec08b0b96dcc48

--- r/PARConnector/functionalTests/testPAConnect.r
+++
@@ -1,8 +0,0 @@
-source("functionalTests/utils.r")
-
-library("PARConnector");
-
-cat("*** Trying to connect to http://localhost:8080/rest/rest ***","\n") 
-
-PAConnect(url='http://localhost:8080/rest/rest', login='demo', pwd='demo');
-cat("Sucessfully connected !!", "\n");
