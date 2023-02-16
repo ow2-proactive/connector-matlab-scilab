6254508cbfd4b9d701005b0c60a6864a0e686f56
Oana Schiopu <oana-schiopu@activeeon.com>
2023-02-16 11:17:23 +0100

    refactor-call-to-jobFilterCriteria-constructor

--- common/matsci-common-middleman/src/main/java/org/ow2/proactive/scheduler/ext/matsci/middleman/SchedulerModel.java
+++ common/matsci-common-middleman/src/main/java/org/ow2/proactive/scheduler/ext/matsci/middleman/SchedulerModel.java
@@ -402,6 +402,7 @@ public class SchedulerModel {
                                                                          null,
                                                                          null,
                                                                          null,
+                                                                         null,
                                                                          null),
                                                    JOB_SORT_PARAMS)
                                           .getList();
